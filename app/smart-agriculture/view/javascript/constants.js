6dd921fcc282f1730400f657399080e21376d966
NadimScriptr <53814203+NadimScriptr@users.noreply.github.com>
2019-10-30 16:57:28 +0100

    CSS modifications on Login, map, header & menu

--- app/smart-agriculture/view/javascript/constants.js
+++ app/smart-agriculture/view/javascript/constants.js
@@ -3,7 +3,7 @@ myApp.constant("constants", {
  logo: "https://i.ibb.co/TmfS643/logo-450.png",
  login: {
     "background": '', // '//s3.amazonaws.com/scriptr-cdn/carwash/images/background-3.jpg',
-    "logo": '//i.ibb.co/TmfS643/logo-450.png',
+    "logo": '//convergia-cdn.s3.amazonaws.com/logo-450.png',
     "poweredBy": '//scriptr-cdn.s3.amazonaws.com/carwash/images/powered-by-scriptr.png'
   },
    sources : {
